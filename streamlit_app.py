6d758d0edc066189867eb9b83e9866b2c48ef6d0
Jafar Vohra <96442226+jafarvohra@users.noreply.github.com>
2024-03-27 01:49:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,15 @@
-pip install boto3
+def install_and_import(package):
+    import importlib
+    try:
+        importlib.import_module(package)
+    except ImportError:
+        import pip
+        pip.main(['install', package])
+    finally:
+        globals()[package] = importlib.import_module(package)
+
+
+install_and_import('boto3')
 
 import streamlit as st
 import requests
